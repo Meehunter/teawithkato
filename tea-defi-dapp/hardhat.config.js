5bd5cba700db17f471c3cd7ada583ab03d660d83
Meehunter <85058759+Meehunter@users.noreply.github.com>
2025-04-13 20:13:23 +0000

    Recreate contracts folder

---
+++ tea-defi-dapp/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.20",
+    networks: {
+        teaSepolia: {
+              url: "https://tea-sepolia.g.alchemy.com/public",
+                    accounts: [], // Tidak perlu private key
+                          chainId: 10218,
+                              },
+                                },
+                                };
+
+
+
+  
+
